58f63ba4a691de285a39cf2c00deb1654bd882a9
Дмитрий Болдин <dimkahs658@gmail.com>
2024-02-15 18:35:58 +0300

    запрограммирована передача в UART

--- src/main.cpp
+++ src/main.cpp
@@ -1,73 +1,126 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/timer.h>
-
-
 #include <libopencm3/cm3/nvic.h> //заголовок для перерывааний находиться в другой папке 
 
-constexpr uint16_t PERIOD_MS{1000};
-
-void blink_LED() 
+void setup()
 {
-if(timer_get_counter(TIM1) < PERIOD_MS / 2)   
-  {
-    gpio_set(GPIOE, GPIO9);  
-  } 
+// Интерфейс U(S)ART с внешним миром
+rcc_periph_clock_enable(RCC_GPIOA);                           // Разморозка порта ввода/вывода
+
+gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO9 | GPIO10);  // Режим альтернативной функции
+gpio_set_af(GPIOA,GPIO_AF7, GPIO9 | GPIO10);                           // Альтернативная функция (выбор по номеру) PA9 --- Tx, PA10 --- Rx.
 
 
-else 
-  {
-  gpio_clear(GPIOE, GPIO9);
-  } 
+// Настройка и включение U(S)ART, номер устройства настраивается
+// для stm32f303 --- в диапазоне 1 -- 5; 4 и 5 --- UART.
+// для stm32f407 --- в диапазоне 1 -- 6; 4 и 5 --- UART.
+
+rcc_periph_clock_enable(RCC_USART1);                      // Разморозка ПУ
+
+usart_set_baudrate(USART1, 115200);                       // Скорость передачи
+usart_set_databits(USART1, 8);                            // Размер посылки
+usart_set_stopbits(USART1, USART_STOPBITS_1);             // Количество стоп-битов
+usart_set_parity(USART1, USART_PARITY_NONE);              // Контроль четности
+
+usart_set_mode(USART1, USART_MODE_TX_RX);                 // Режим работы ПУ
+usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);   // Управление процессом передачи сообщений
+
+usart_enable(USART1);                                     // Включение ПУ
+
 
 }
 
-
-void LED_gpio_setup()
+void loop()
 {
-  rcc_periph_clock_enable( RCC_GPIOE);
-  gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9 | GPIO11  | GPIO14 );
 
-  gpio_mode_setup(GPIOE, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO14 );
 
-  gpio_set_af(GPIOE, GPIO_AF2, GPIO14);
+usart_send_blocking(USART1, 0x55);
+usart_send_blocking(USART1, 'H');
+usart_send_blocking(USART1, 'e');
+usart_send_blocking(USART1, 'l');
+usart_send_blocking(USART1, 'l');
+usart_send_blocking(USART1, 'o');
+usart_send_blocking(USART1, '\n');
+usart_send_blocking(USART1, '\r');
+
+for(volatile uint32_t i=0 ; i<1000; i++); 
 
 }
-
-void timer_setup()
-{
-  rcc_periph_clock_enable( RCC_TIM1);
-  timer_set_prescaler (TIM1, rcc_get_timer_clk_freq(TIM1) / PERIOD_MS -1  );
-  timer_set_period(TIM1, PERIOD_MS-1);
-
-//для разрешения беспокойства процессора 
-  timer_enable_irq(TIM1, TIM_DIER_UIE);
-  nvic_enable_irq (NVIC_TIM1_UP_TIM16_IRQ ); 
-
-  timer_enable_counter(TIM1);
-}
-
-
-
 
 int main()
-{ //  насттройка порта ввода-вывода 
-  //Для разгоник ЦП нужно порабоать с rcc_clock_setup_pll
-  rcc_clock_setup_pll( &rcc_hsi_configs[RCC_CLOCK_HSI_64MHZ]);
+{
+  setup();
 
-    rcc_periph_clock_enable( RCC_GPIOE);
-  gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO10);
-  gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO12);
+  while(true)
+  {
+    loop();
+  }
+}
+
+// constexpr uint16_t PERIOD_MS{1000};
+
+// void blink_LED() 
+// {
+// if(timer_get_counter(TIM1) < PERIOD_MS / 2)   
+//   {
+//     gpio_set(GPIOE, GPIO9);  
+//   } 
+
+
+// else 
+//   {
+//   gpio_clear(GPIOE, GPIO9);
+//   } 
+
+// }
+
+
+// void LED_gpio_setup()
+// {
+//   rcc_periph_clock_enable( RCC_GPIOE);
+//   gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9 | GPIO11  | GPIO14 );
+
+//   gpio_mode_setup(GPIOE, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO14 );
+
+//   gpio_set_af(GPIOE, GPIO_AF2, GPIO14);
+
+// }
+
+// void timer_setup()
+// {
+//   rcc_periph_clock_enable( RCC_TIM1);
+//   timer_set_prescaler (TIM1, rcc_get_timer_clk_freq(TIM1) / PERIOD_MS -1  );
+//   timer_set_period(TIM1, PERIOD_MS-1);
+
+// //для разрешения беспокойства процессора 
+//   timer_enable_irq(TIM1, TIM_DIER_UIE);
+//   nvic_enable_irq (NVIC_TIM1_UP_TIM16_IRQ ); 
+
+//   timer_enable_counter(TIM1);
+// }
 
 
 
 
-LED_gpio_setup();
-timer_setup();
+// int main()
+// { //  насттройка порта ввода-вывода 
+//   //Для разгоник ЦП нужно порабоать с rcc_clock_setup_pll
+//   rcc_clock_setup_pll( &rcc_hsi_configs[RCC_CLOCK_HSI_64MHZ]);
 
-  while (true )
- {
-  blink_LED();
+//     rcc_periph_clock_enable( RCC_GPIOE);
+//   gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO11);
+//   gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO13);
+
+
+
+
+// LED_gpio_setup();
+// timer_setup();
+
+//   while (true )
+//  {
+//   blink_LED();
 
 
 
@@ -81,19 +134,19 @@ timer_setup();
 //   for(volatile uint32_t i = 0; i < 1'000'000; ++i);
 // gpio_toggle(GPIOE, GPIO12);
   
-  }
+//   }
 
-}
+// }
 
 
 
-void tim1_up_tim16_isr(void)  //обработчик  
-{
+// void tim1_up_tim16_isr(void)  //обработчик  
+// {
 
-timer_clear_flag(TIM1, TIM_SR_UIF); // снимаем заброс или выключаем будильник.  TIM_SR_UIF- для обнуленяи
-//полезная работа те переключение сетодиода  
-gpio_toggle(GPIOE, GPIO11) ;
-}
+// timer_clear_flag(TIM1, TIM_SR_UIF); // снимаем заброс или выключаем будильник.  TIM_SR_UIF- для обнуленяи
+// //полезная работа те переключение сетодиода  
+// gpio_toggle(GPIOE, GPIO11) ;
+// }
 
 
 
